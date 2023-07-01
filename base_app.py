a6af645b87a19d8861b9493d511ddc972f96d99d
CJay <chijiokennabueze@gmail.com>
2023-07-01 10:46:26 +0100

    tested updated commit

--- base_app.py
+++ base_app.py
@@ -27,31 +27,28 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
-
-from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem import WordNetLemmatizer
 
-
-# DATA CLEANING & FEATURE ENGINEERING
-def data_cleaning(data):
-    copy_data = data.copy()
-
-    # Convert tweets to lowercase
-    copy_data['message'] = copy_data['message'].apply(lambda x: x.lower())
-
-    # Tokenizing the tweets
-    copy_data['message'] = copy_data['message'].apply(lambda x:[word for word in x.split(" ")])
-    
-    # Initialize a lemmatizer object
-    lemmatizer = WordNetLemmatizer()
-    copy_data['message'] = copy_data['message'].apply(
-        lambda x: ' '.join([lemmatizer.lemmatize(i, pos='v') for i in x]))
-    
-    return copy_data
-
+# Vectorizer
+news_vectorizer = open("resources/pipeline.pkl","rb")
+tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
+
+# DATA CLEANING
+def text_cleaning(text):
+    # Convert tweets to lowercase
+    text = text.lower()
+
+    # Tokenizing the tweets
+    text = text.split(" ")
+    
+    # Initialize a lemmatizer object
+    lemmatizer = WordNetLemmatizer()
+    text = ' '.join([lemmatizer.lemmatize(i, pos='v') for i in text])
+    
+    return text
 
 # The main function where we will build the actual app
 def main():
@@ -81,35 +78,24 @@ def main():
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
-		tweet_text = [st.text_area("Enter Text","Type Here")]
+		tweet_text = st.text_area("Enter Text","Type Here")
 
-		model_options = ["Logistic Regression", "Linear SVC", "Multinomial Naive Bayes"]
-		model_selection = st.selectbox("Select Model", model_options)
-		if model_selection == "Logistic Regression":
-			selected_model = "lr"
-		elif model_selection == "Linear SVC":
-			selected_model = "lr_svc"
-		else:
-			selected_model = "mnb"
+		# Load your .pkl files into a dictionary
+		models = {
+			"Logistic regression": "lr.pkl",
+			"Multinomial naive bayes": "mnb.pkl",
+			"Linear SVC": "lr_svc.pkl"
+		}
+		# Create a selectbox to let the user choose the model
+		model_selection = st.selectbox("Select Model", list(models.keys()))
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
-			tweet_df = pd.DataFrame({"message": tweet_text})
-
-			vectorizer = CountVectorizer(
-				analyzer = 'word', 
-				tokenizer = None, 
-				preprocessor = None, 
-				stop_words = None, 
-				max_features = 180000,
-				min_df = 1,
-				ngram_range = (1, 2)
-			)
-	
-			vect_text = vectorizer.transform(data_cleaning(tweet_df)["message"])
+			vect_text = tweet_cv.transform([text_cleaning(tweet_text)]).toarray()
+		
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join(f"pickle_files/{selected_model}.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join(f"resources/{models[model_selection]}"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
@@ -120,3 +106,5 @@ def main():
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
+
+	
