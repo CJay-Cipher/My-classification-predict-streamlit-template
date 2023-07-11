d9c15a1736895a0da36ed8927a2e38f2094eb408
CJay <chijiokennabueze@gmail.com>
2023-07-11 17:28:20 +0100

    new updated commit

--- base_app.py
+++ base_app.py
@@ -27,28 +27,13 @@ import joblib,os
 
 # Data dependencies
 import pandas as pd
-from nltk.stem import WordNetLemmatizer
 
 # Vectorizer
-news_vectorizer = open("resources/pipeline.pkl","rb")
+news_vectorizer = open("resources/CountVectorizer.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
-
-# DATA CLEANING
-def text_cleaning(text):
-    # Convert tweets to lowercase
-    text = text.lower()
-
-    # Tokenizing the tweets
-    text = text.split(" ")
-    
-    # Initialize a lemmatizer object
-    lemmatizer = WordNetLemmatizer()
-    text = ' '.join([lemmatizer.lemmatize(i, pos='v') for i in text])
-    
-    return text
 
 # The main function where we will build the actual app
 def main():
@@ -56,7 +41,7 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
+	st.title("Tweet Classifier")
 	st.subheader("Climate change tweet classification")
 
 	# Creating sidebar with selection box -
@@ -68,7 +53,7 @@ def main():
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+		st.markdown("This section contains tweets pertaining to climate change collected between Apr 27, 2015 and Feb 21, 2018.")
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
@@ -79,25 +64,33 @@ def main():
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
-
+        
+        
 		# Load your .pkl files into a dictionary
-		models = {
-			"Logistic regression": "lr.pkl",
-			"Multinomial naive bayes": "mnb.pkl",
-			"Linear SVC": "lr_svc.pkl"
-		}
-		# Create a selectbox to let the user choose the model
-		model_selection = st.selectbox("Select Model", list(models.keys()))
+		model_files = {"Logistic regression": "lr.pkl","support vector classifier": "lr_svc.pkl","multinomial naive baye": "mnb.pkl"}
+
+		 # Create a selectbox to let the user choose the model
+		selected_model = st.selectbox("Select Model", list(model_files.keys()))
 
 		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([text_cleaning(tweet_text)]).toarray()
-		
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join(f"resources/{models[model_selection]}"),"rb"))
-			prediction = predictor.predict(vect_text)
+			 # Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
 
+			# Load the selected model
+			model_path = os.path.join("resources", model_files[selected_model])
+			predictor = joblib.load(open(model_path, "rb"))
+
+			# Make predictions
+			prediction = predictor.predict(vect_text)
+			if prediction == 0:
+				prediction = "Neutral"
+			elif prediction == 1:
+				prediction = "Pro"
+			elif prediction == -1:
+				prediction = "Anti"
+			else:
+				prediction = "News"
+			
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
@@ -106,5 +99,3 @@ def main():
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
-
-	
